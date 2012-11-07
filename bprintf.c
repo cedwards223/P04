b429812541335a0b3296a742ee93a261c4e1a51c
Chris Edwards <cmedwards@coe.edu>
2012-11-07 15:53:45 -0600

    Revert "Added stuff"
    This reverts commit 229ea646a19f462853ac57241acea385a0e62ab4.

--- bprintf.c
+++ bprintf.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stlio>
+#include <stdlib>
+#include <string>
 
 #define BUFFSIZE 64
 
 void init_bprintf();
-int bprintf(const char *output);
+int bprintf(const char);
 char buffer[BUFFSIZE];
-int more = 0; // Keep track of where the buffer is with no end line
 
 void init_bprintf()
 {
@@ -16,40 +15,16 @@ void init_bprintf()
 
 int bprintf(const char *output)
 {
-   // while (output != EOF) // Check for end character
-   // {        
-        for (more; more < BUFFSIZE-1; more++)
-        {   
-            buffer[more] = output;
-         //   if(buffer[i] == "\0") // Break out if EOF)
-         //     break;
-            if(buffer[more] == "\n")// Break out if end of line
-                break;
-        }
-        if( more == BUFFSIZE-1)
-        {
-            int x;
-            char c;
-            for(x= 0; buffer[x] !="\0"; x++) // Dump buffer
-            {
-                c = buffer[x];
-                if (c = "\n")
-                    printf("\n");
-                else if (c = "\0")
-                    break;
-                else
-                    printf("%c" ,c);
-            }
-            more = 0;
-        }
-       // else
-         //   break;
-   // }
+    for (int i = 0; buffer[i-1] != "\0"; i++)
+    {
+        
+    }
+
 }
 
 int main()
 {
     init_bprintf();
-    bprintf("buffer");
+
 
 }
