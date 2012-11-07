7cd968fd142451456f5dcbcc99eaebb8d0025b35
Chris Edwards <cmedwards@coe.edu>
2012-11-07 15:58:52 -0600

    Works
    Final

--- bprintf.c
+++ bprintf.c
@@ -1,12 +1,22 @@
-#include <stlio>
-#include <stdlib>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define BUFFSIZE 64
+#define BUFSIZE 64
 
 void init_bprintf();
-int bprintf(const char);
-char buffer[BUFFSIZE];
+int bprintf(const char *);
+void writebuf();
+
+char *buffer;
+int buffermark = 0;
+
+int main()
+{
+    buffer = malloc(BUFSIZE);
+    init_bprintf();
+    bprintf("Hello world!so what dlfajsfljasl;dfja;lsdjf;l asdf;kasjdl;fkjasd l;fj ;sdfjasld jfk;asdjf ;kashf;lj asdkl;f  sdfgadfcvgnukiujnhtrfedwf5hu\n");
+}
 
 void init_bprintf()
 {
@@ -15,16 +25,39 @@ void init_bprintf()
 
 int bprintf(const char *output)
 {
-    for (int i = 0; buffer[i-1] != "\0"; i++)
+    char next;
+    int i =0;
+    while(next != '\0')
     {
-        
+        if(buffermark == 64)
+        {
+            writebuf();
+            buffermark = 0;  
+        }
+        else if((next = output[i]) == '\n')
+        {
+            buffer[buffermark] = next;
+            writebuf();
+            buffermark = 0;
+        }
+        else
+        {
+            buffer[buffermark] = next;
+            buffermark++;
+        }
+        i++;
     }
-
+        
 }
 
-int main()
+void writebuf()
 {
-    init_bprintf();
-
-
+    int x = 0;
+    while(x <= buffermark)
+    {
+        printf("%c", buffer[x]);
+        x++;
+    }
+    sleep(1);
+    buffermark = 0;
 }
