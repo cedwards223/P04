229ea646a19f462853ac57241acea385a0e62ab4
Chris Edwards <cmedwards@coe.edu>
2012-11-06 21:56:27 -0600

    Added stuff
    Make file

--- bprintf.c
+++ bprintf.c
@@ -1,12 +1,13 @@
-#include <stlio>
-#include <stdlib>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BUFFSIZE 64
 
 void init_bprintf();
-int bprintf(const char);
+int bprintf(const char *output);
 char buffer[BUFFSIZE];
+int more = 0; // Keep track of where the buffer is with no end line
 
 void init_bprintf()
 {
@@ -15,16 +16,40 @@ void init_bprintf()
 
 int bprintf(const char *output)
 {
-    for (int i = 0; buffer[i-1] != "\0"; i++)
-    {
-        
-    }
-
+   // while (output != EOF) // Check for end character
+   // {        
+        for (more; more < BUFFSIZE-1; more++)
+        {   
+            buffer[more] = output;
+         //   if(buffer[i] == "\0") // Break out if EOF)
+         //     break;
+            if(buffer[more] == "\n")// Break out if end of line
+                break;
+        }
+        if( more == BUFFSIZE-1)
+        {
+            int x;
+            char c;
+            for(x= 0; buffer[x] !="\0"; x++) // Dump buffer
+            {
+                c = buffer[x];
+                if (c = "\n")
+                    printf("\n");
+                else if (c = "\0")
+                    break;
+                else
+                    printf("%c" ,c);
+            }
+            more = 0;
+        }
+       // else
+         //   break;
+   // }
 }
 
 int main()
 {
     init_bprintf();
-
+    bprintf("buffer");
 
 }
